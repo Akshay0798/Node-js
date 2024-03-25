cadf50ae0186f5d34608b5ce2cb101893ba57853
Akshay Umredkar <94985332+Akshay0798@users.noreply.github.com>
2024-03-25 21:28:26 +0530

    middleware

--- project-01/index.js
+++ project-01/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const mongoose = require("mongoose");
 const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 8000;
