f2eb0822ab067fb7924b582d8b5943dd27149cf7
Akshay Umredkar <94985332+Akshay0798@users.noreply.github.com>
2024-03-25 22:46:10 +0530

    mongoDB connected

--- project-01/index.js
+++ project-01/index.js
@@ -1,3 +1,4 @@
+// Import required modules
 const express = require("express");
 const fs = require("fs");
 const mongoose = require("mongoose");
@@ -5,87 +6,123 @@ const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 8000;
 
-// Middleware - Plugin (plugin ne data uthaya which I write in "postman/POST/Body/x-www-form-urlencoded", javascript object create kiya and "req.body" ke ander dal diya)
+// Connect to MongoDB
+mongoose
+    .connect("mongodb://127.0.0.1:27017/youtube-app-1")
+    .then(() => console.log("MongoDB connected")) // Returns promises
+    .catch((err) => console.log("Mongo Error", err));
+
+// Define Schema
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    jobTitle: {
+        type: String,
+    },
+    gender: {
+        type: String,
+    },
+});
+const User = mongoose.model("user", userSchema);
+
+// Middleware setup
 app.use(express.urlencoded({ extended: false }));
-// Date 23/03/2024 - Express Middleware
-app.use((req, res, next) => {
-  console.log("Hello from middleware 1");
-  req.myUserName = 'ak.umredkar';
-  //! return res.json({ message : "Hello from middleware 1" })
-  next()
-})
-app.use((req, res, next) => {
-  fs.appendFile('log.txt',`\n${Date.now()} : ${req.ip} | ${req.method} -> ${req.path}`,(err,data)=>{
-    next()
-  })
-  //! console.log("Hello from middleware 2",req.myUserName);
-})
 
+// Middleware for logging
+app.use((req, res, next) => {
+    console.log("Hello from middleware 1");
+    req.myUserName = "ak.umredkar";
+    next();
+});
+app.use((req, res, next) => {
+    fs.appendFile(
+        "log.txt",
+        `\n${Date.now()} : ${req.ip} | ${req.method} -> ${req.path}`,
+        (err, data) => {
+            next();
+        }
+    );
+});
 
-// Routes
-// Route to get a list of users in HTML format
+// Define Routes
+
+// HTML format route for user list
 app.get("/users", (req, res) => {
-  const html = `<ul>${users.map((user) => `<li>${user.first_name}</li>`).join("")}</ul>`;
-  return res.send(html);
+    const html = `<ul>${users
+        .map((user) => `<li>${user.first_name}</li>`)
+        .join("")}</ul>`;
+    return res.send(html);
 });
 
-// REST API
-// Route to get all users in JSON format
+// JSON format route for all users
 app.get("/api/users", (req, res) => {
-  //! console.log("I am in get route",req.myUserName)
-   res.setHeader("X-MyName","Akshay Umredkar"); //custom header - Always add X in custom header
-   console.log(req.headers);
-  return res.json(users);
+    res.setHeader("X-MyName", "Akshay Umredkar");
+    console.log(req.headers);
+    return res.json(users);
 });
 
-// Route to get, create, update, or delete a specific user
+// CRUD Operations for a specific user
 
-// Handling POST request to create a new user
+// Create a new user
 app.post("/api/users", (req, res) => {
-  // Logic to add a new user
-  const body = req.body; // whatever data we send from frontend it will store in body
-  //! console.log("Body",body); //it will show 'undefine' bcoz express dont know what kind of data it is and how to handle so for that we use middleware
-  users.push({ ...body, id: users.length + 1 });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "Success", id: users.length });
-  });
+    const body = req.body;
+    users.push({ ...body, id: users.length + 1 });
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({ status: "Success", id: users.length });
+    });
 });
+
+// Get, update, or delete a specific user
 app
-  .route("/api/users/:id")
-  .get((req, res) => {
-    const userId = Number(req.params.id); // "req.params.id" is used to access route parameters in Express.js.
-    const foundUser = users.find((user) => user.id === userId);
-    return res.json(foundUser);
-  })
-  // Handling PATCH request to edit a user
-  .patch((req, res) => {
-    // Logic to edit user with id
-    const userId = Number(req.params.id); // "req.params.id" is used to access route parameters in Express.js.
-    const updatedUserData = req.body;
-    const userIndex = users.findIndex((user) => user.id === userId);
-    if (userIndex === -1) {
-      return res.status(404).json({ status: "Error", message: "User not found" });
-    }
-    // Update user's data
-    users[userIndex] = { ...users[userIndex], ...updatedUserData };
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-      return res.json({status: "Success",message: "User updated successfully"});
+    .route("/api/users/:id")
+    .get((req, res) => {
+        const userId = Number(req.params.id);
+        const foundUser = users.find((user) => user.id === userId);
+        return res.json(foundUser);
+    })
+    .patch((req, res) => {
+        const userId = Number(req.params.id);
+        const updatedUserData = req.body;
+        const userIndex = users.findIndex((user) => user.id === userId);
+        if (userIndex === -1) {
+            return res
+                .status(404)
+                .json({ status: "Error", message: "User not found" });
+        }
+        users[userIndex] = { ...users[userIndex], ...updatedUserData };
+        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+            return res.json({
+                status: "Success",
+                message: "User updated successfully",
+            });
+        });
+    })
+    .delete((req, res) => {
+        const userId = Number(req.params.id);
+        const userIndex = users.findIndex((user) => user.id === userId);
+        if (userIndex === -1) {
+            return res
+                .status(404)
+                .json({ status: "Error", message: "User not found" });
+        }
+        users.splice(userIndex, 1);
+        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+            return res.json({
+                status: "Success",
+                message: "User deleted successfully",
+            });
+        });
     });
-  })
-  // Handling DELETE request to delete a user
-  .delete((req, res) => {
-    // Logic to delete user with id
-    const userId = Number(req.params.id);
-    const userIndex = users.findIndex((user) => user.id === userId);
-    if (userIndex === -1) {
-      return res.status(404).json({ status: "Error", message: "User not found" });
-    }
-    // Remove user from the array
-    users.splice(userIndex, 1);
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-      return res.json({status: "Success",message: "User deleted successfully"});
-    });
-  });
 
 // Start the server
 app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
