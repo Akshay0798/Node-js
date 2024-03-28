4659847dc26afe5fb225bc418bef5388e639eb8e
Akshay Umredkar <94985332+Akshay0798@users.noreply.github.com>
2024-03-28 15:29:31 +0530

    CRUD Operaton by using DB

--- project-01/index.js
+++ project-01/index.js
@@ -2,37 +2,41 @@
 const express = require("express");
 const fs = require("fs");
 const mongoose = require("mongoose");
-const users = require("./MOCK_DATA.json");
+// const users = require("./MOCK_DATA.json"); //comment bcoz i need user list from Databae
+const { timeStamp } = require("console");
 const app = express();
-const PORT = 9999;
+const PORT = 9000;
 
 // Connect to MongoDB
 mongoose
-  .connect("mongodb://127.0.0.1:27017/Facebook-App-1") // "Facebook-app-1" DataBase Name
+  .connect("mongodb://127.0.0.1:27017/youtube-app-1") // "Facebook-app-1" DataBase Name
   .then(() => console.log("MongoDB connected")) // Returns promises
   .catch((err) => console.log("Mongo Error", err));
 
 // Define Schema
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    jobTitle: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  jobTitle: {
-    type: String,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 //creating a Model
 //By using User i will use Mongo
 const User = mongoose.model("user", userSchema);
@@ -61,44 +65,84 @@ app.use((req, res, next) => {
 // Define Routes
 
 // HTML format route for user list
-app.get("/users", (req, res) => {
-  const html = `<ul>${users
-    .map((user) => `<li>${user.first_name}</li>`)
-    .join("")}</ul>`;
+// app.get("/users", (req, res) => {
+//   const html = `<ul>${users
+//     .map((user) => `<li>${user.first_name}</li>`)
+//     .join("")}</ul>`;
+//   return res.send(html);
+// });
+
+//* HTML format route for user list from MongoDB Database
+app.get("/users", async (req, res) => {
+  const allDBUsers = await User.find({});
+  const html = `<ul>
+  ${allDBUsers
+    .map((user) => `<li>${user.firstName} - ${user.email}</li>`)
+    .join("")}
+  </ul>`;
   return res.send(html);
 });
 
 // JSON format route for all users
-app.get("/api/users", (req, res) => {
-  //always add X to custom header
-  res.setHeader("X-MyName", "Akshay Umredkar"); //custon header
-  console.log(req.headers);
-  return res.json(users);
+// app.get("/api/users", (req, res) => {
+//   //always add X to custom header
+//   res.setHeader("X-MyName", "Akshay Umredkar"); //custon header
+//   console.log(req.headers);
+//   return res.json(users);
+// });
+
+//* JSON format route for all users from MongoDB DataBase
+app.get("/api/users", async (req, res) => {
+  const allDBUsers = await User.find({});
+  return res.json(allDBUsers);
 });
 
 // CRUD Operations for a specific user
 
 // Create a new user
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async (req, res) => {
   const body = req.body;
   if (
     !body ||
-    !first_name.body ||
-    !last_name.body ||
-    !email.body ||
-    !gender.body ||
-    !job_title.body
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
   ) {
-    return res.status(400).json({ message: "All Field are required..." });
+    return res.status(400).json({ message: "All fields are required." });
   }
-  users.push({ ...body, id: users.length + 1 });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.status(201).json({ status: "Success", id: users.length });
-  });
+
+  // Create user in MongoDB
+  try {
+    const result = await User.create({
+      firstName: body.first_name,
+      lastName: body.last_name,
+      email: body.email,
+      gender: body.gender,
+      jobTitle: body.job_title,
+    });
+    console.log("Result", result);
+    return res.status(201).json({ message: "Success" });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return res.status(500).json({ message: "Internal server error." });
+  }
+
+  // const newUser = { ...body, id: users.length + 1 };
+  // users.push(newUser);
+
+  // fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+  //   if (err) {
+  //     console.error("Error writing to file:", err);
+  //     return res.status(500).json({ message: "Internal server error." });
+  //   }
+  //   return res.status(201).json({ status: "Success", id: newUser.id });
+  // });
 });
 
 // Get, update, or delete a specific user
-app
+/*app
   .route("/api/users/:id")
   .get((req, res) => {
     const userId = Number(req.params.id);
@@ -140,6 +184,80 @@ app
       return res.status(200).json({
         status: "Success",
         message: "User deleted successfully",
+      });
+    });
+  });
+*/
+
+// Get, update, or delete a specific user From MongoDB DataBase
+app
+  .route("/api/users/:id")
+  .get(async (req, res) => {
+    const userId = await User.findById(req.params.id);
+    if (!userId) {
+      return res
+        .status(404)
+        .json({ status: "Error", message: "User not found" });
+    }
+    return res.json(userId);
+  })
+  .patch(async(req, res) => {
+    const userId = req.params.id;
+    const updatedUserData = req.body;
+
+    // Update user in the MongoDB database
+    await User.findByIdAndUpdate(userId, updatedUserData, { new: true }).then(
+      (updatedUser) => {
+        // Check if user was found and updated
+        if (!updatedUser) {
+          return res
+            .status(404)
+            .json({ status: "Error", message: "User not found" });
+        }
+
+        return res.status(200).json({
+          status: "Success",
+          message: "User updated successfully",
+          user: updatedUser,
+        });
+      }
+    );
+  })
+  // .patch(async (req, res) => {
+  //   const userId = req.params.id;
+  //   const newFirstName = "MyHotu"; // Hardcoded new first name value
+  //   // Update user's first name in the MongoDB database
+  //   await User.findByIdAndUpdate(
+  //     userId,
+  //     { firstName: newFirstName },
+  //     { new: true }
+  //   ).then((updatedUser) => {
+  //     if (!updatedUser) {
+  //       return res
+  //         .status(404)
+  //         .json({ status: "Error", message: "User not found" });
+  //     }
+  //     return res.status(200).json({
+  //       status: "Success",
+  //       message: "User updated successfully",
+  //       user: updatedUser,
+  //     });
+  //   });
+  // })
+  .delete(async (req, res) => {
+    const userId = req.params.id;
+    await User.findByIdAndDelete(userId).then((deletedUser) => {
+      // Check if user was found and deleted
+      if (!deletedUser) {
+        return res
+          .status(404)
+          .json({ status: "Error", message: "User not found" });
+      }
+
+      return res.status(200).json({
+        status: "Success",
+        message: "User deleted successfully",
+        user: deletedUser,
       });
     });
   });
